a89cd5fbb5f9430db7c58196ae0516fa38491ba5
Donghwan Seo <tonky0110@gmail.com>
2020-11-01 12:59:56 +0900

    2.3 TypeOrm setup on app.module.ts

--- README.md
+++ README.md
@@ -6,11 +6,18 @@ The Backend of Nuber Eats Clone
 
 ### install package
 
+[backend server]
+
 - [V] npm i @nestjs/graphql graphql-tools graphql apollo-server-express
 - [V] npm i class-validator class-transformer
+
+[DB]- [V]
+
+- [V]
 
 ## Database - postgrasql
 
 ### install
 
-www.postgrasql.com > download && postico install(mac) or pgAdmin(windows)
+- www.postgrasql.com > download && postico install(mac) or pgAdmin(windows)
+- /du; - 사용자 권한 확인.
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,25 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   imports: [
+    RestaurantsModule,
+    TypeOrmModule.forRoot({
+      type: '',
+      host: '',
+      port: ,
+      username: '',
+      password: '',
+      database: '',
+      synchronize: true,
+      logging: true,
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
-    RestaurantsModule,
   ],
   controllers: [],
   providers: [],

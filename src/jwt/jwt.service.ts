99cfb714e0f22e6a4a4a8100e07dff339186c535
Donghwan Seo <tonky0110@gmail.com>
2020-11-03 23:49:33 +0900

    5.4 JwtModule 및 interface, config생성 후 module, service에서 import 해서 사용(불필요한 하드코딩방지 및 오타 방지)

--- src/jwt/jwt.service.ts
+++ src/jwt/jwt.service.ts
@@ -1,7 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { optional } from 'joi';
+import { CONFIG_OPTIONS } from './jwt.constains';
+import { JwtModuleOptions } from './jwt.interfaces';
 
 @Injectable()
 export class JwtService {
+  constructor(
+    @Inject(CONFIG_OPTIONS) private readonly options: JwtModuleOptions,
+  ) {
+    // console.log(options);
+  }
   hello() {
     console.log('Hello');
   }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { UsersService } from './user.service';

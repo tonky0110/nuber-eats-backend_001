3e7ed46d0496561683132f2a7885d6b73484c7be
Donghwan Seo <tonky0110@gmail.com>
2020-11-04 22:30:44 +0900

    5.5 JWT SERVICE - jwtModule을 전연변수로 만들어서 다른 모듈/서비스에서 사용할 수 있도록 설정.

--- src/jwt/jwt.service.ts
+++ src/jwt/jwt.service.ts
@@ -1,4 +1,5 @@
 import { Inject, Injectable } from '@nestjs/common';
+import * as jwt from 'jsonwebtoken';
 import { optional } from 'joi';
 import { CONFIG_OPTIONS } from './jwt.constains';
 import { JwtModuleOptions } from './jwt.interfaces';
@@ -10,7 +11,7 @@ export class JwtService {
   ) {
     // console.log(options);
   }
-  hello() {
-    console.log('Hello');
+  sign(payload: object): string {
+    return jwt.sign(payload, this.options.privateKey);
   }
 }
